c0e2efa5fa220e4c1de7fe5ec6ff53c5a8c11c24
Gökay Şatır <gokaysatir@collabora.com>
2020-12-25 19:42:24 +0300

    CanvasTileLayer: Add tiles section.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: I99e404a5f93f629134c5898133b78e3f75e64123

--- loleaflet/src/layer/tile/CanvasTileLayer.js
+++ loleaflet/src/layer/tile/CanvasTileLayer.js
@@ -552,6 +552,8 @@ L.CanvasTileLayer = L.TileLayer.extend({
 		this._map.on('move', this._syncTilePanePos, this);
 
 		this._map.on('viewrowcolumnheaders', this._updateRenderBackground, this);
+
+		this._initSectionContainer();
 	},
 
 	_syncTilePanePos: function () {
@@ -561,6 +563,48 @@ L.CanvasTileLayer = L.TileLayer.extend({
 			L.DomUtil.setPosition(tilePane, new L.Point(-mapPanePos.x , -mapPanePos.y));
 		}
 	},
+
+	_initSectionContainer: function () {
+		// SectionContainer can't reach events. They are handled by tilepane.
+		this._sectionContainer = new CanvasSectionContainer(this._canvas);
+
+
+
+		this._addTilesSection();
+	},
+
+	_addTilesSection: function () {
+		var that = this;
+		var added = this._sectionContainer.addSection({
+			name: 'tiles',
+			anchor: 'top left',
+			position: [250 * that._dpiScale, 250 * that._dpiScale], // Set its initial position to somewhere blank. Other sections shouldn't cover this point after initializing.
+			size: [0, 0], // Going to be expanded, no initial width or height is necessary.
+			expand: 'top left bottom right', // Expand to all directions.
+			drawingOrder: 1,
+			zIndex: 1,
+			interactable: true,
+			myProperties: { docLayer: that },
+			onMouseMove: function () { },
+			onMouseDown: function () {},
+			onMouseUp: function () {},
+			onClick: function () {},
+			onDoubleClick: function () {},
+			onMouseWheel: function () {},
+			onLongPress: function () {},
+			onMultiTouchStart: function () {},
+			onMultiTouchMove: function () {},
+			onMultiTouchEnd: function () {},
+			onResize: function () {},
+			onNewDocumentTopLeft: function () {},
+			onDraw: function () {}
+		});
+
+		if (added) {
+			console.log('test');
+		}
+	},
+
 
 	_updateRenderBackground: function() {
 		var that = this;
